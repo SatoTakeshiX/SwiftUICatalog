807f71d345e5af537d863fed5ea940ab5cd5a93f
satoutakeshi <sato.takeshi1987@gmail.com>
2020-01-17 01:40:01 +0900

    apply inputs outputs

--- CameraSample/CameraSample/ContentView.swift
+++ CameraSample/CameraSample/ContentView.swift
@@ -51,7 +51,7 @@ struct ContentView: View {
                 }
 
                 Button(action: {
-                    self.viewModel.tappedButton()
+                    self.viewModel.apply(.tappedButton)
                 }) {
                     VStack {
                         Image(systemName: "camera")
@@ -93,35 +93,77 @@ struct ContentView_Previews: PreviewProvider {
 
 final class ImagePickerViewModel: ObservableObject, Identifiable {
 
+    private var cancellables: [AnyCancellable] = []
+
+    //MARK: Input
+    enum Inputs {
+        case tappedButton
+        case tappedActionSheet(selectType: UIImagePickerController.SourceType)
+    }
+
+    //MARK: Outputs
     @Published var image: UIImage?
     @Published var isShowActionSheet = false
     @Published var isShowImagePickerView = false
-    private(set) var selectedSourceType: UIImagePickerController.SourceType = .camera
-    private(set) var selectedOption: [PhotoAction] = []
+    @Published private(set) var selectedSourceType: UIImagePickerController.SourceType = .camera
+    @Published private(set) var selectedOption: [PhotoAction] = []
 
-    private(set) lazy var tappedButton: () -> Void = { [weak self] in
-        guard let self = self else { return }
-        self.selectedOption = self.generatePhotoActions()
-        self.isShowActionSheet = true
+    init() {
+        bindOutputs()
+    }
+
+    func apply(_ input: Inputs) {
+        switch input {
+            case .tappedButton:
+                tappedButtonStream.send(())
+            case .tappedActionSheet(let selectType):
+                tappedActionSheet.send(selectType)
+        }
+    }
+    private var tappedButtonStream = PassthroughSubject<Void, Never>()
+    private var tappedActionSheet = PassthroughSubject<UIImagePickerController.SourceType, Never>()
+
+    private func bindOutputs() {
+        let photoActions = tappedButtonStream
+            .map {[weak self] () -> [PhotoAction] in
+                guard let self = self else { return [] }
+                return self.generatePhotoActions()
+        }
+        .assign(to: \.selectedOption, on: self)
+        let isShow = tappedButtonStream
+            .map { () -> Bool in
+                return true
+        }.assign(to: \.isShowActionSheet, on: self)
+
+        let selectedType = tappedActionSheet
+            .assign(to: \.selectedSourceType, on: self)
+        let isShowImagePickerView = tappedActionSheet
+            .map{ type -> Bool in
+                return true
+        }
+        .assign(to: \.isShowImagePickerView, on: self)
+
+        cancellables += [
+            photoActions,
+            isShow,
+            selectedType,
+            isShowImagePickerView
+        ]
     }
 
     private func generatePhotoActions() -> [PhotoAction] {
         var photoActions: [PhotoAction] = []
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
-            let cameraAction = PhotoAction(action: {
-                print("camera")
-                self.isShowImagePickerView = true
-                self.selectedSourceType = .camera
-            }, message: "写真を撮る", imagePickerSourceType: .camera)
+            let cameraAction = PhotoAction(action: { [weak self] in
+                self?.apply(.tappedActionSheet(selectType: .camera))
+                }, message: "写真を撮る")
             photoActions.append(cameraAction)
         }
 
         if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
-            let photoLibraryAction = PhotoAction(action: {
-                print("photoLibraryAction")
-                self.isShowImagePickerView = true
-                self.selectedSourceType = .photoLibrary
-            }, message: "アルバムから選択", imagePickerSourceType: .photoLibrary)
+            let photoLibraryAction = PhotoAction(action: { [weak self] in
+                self?.apply(.tappedActionSheet(selectType: .photoLibrary))
+                }, message: "アルバムから選択")
             photoActions.append(photoLibraryAction)
         }
         return photoActions
@@ -130,6 +172,5 @@ final class ImagePickerViewModel: ObservableObject, Identifiable {
     struct PhotoAction {
         var action: () -> Void
         var message: String
-        var imagePickerSourceType: UIImagePickerController.SourceType
     }
 }
