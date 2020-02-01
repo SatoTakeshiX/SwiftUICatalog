7a25db9f3d3335df148e5e400a0834611cfab50f
satoutakeshi <sato.takeshi1987@gmail.com>
2020-02-01 20:20:28 +0900

    add test

--- GitHubApiClientSample/GitHubApiClientSampleTests/HomeViewModelTests.swift
+++ GitHubApiClientSample/GitHubApiClientSampleTests/HomeViewModelTests.swift
@@ -11,11 +11,52 @@ import XCTest
 
 class HomeViewModelTests: XCTestCase {
 
-    func testHomeViewModel() {
+    func testOnEnter() {
     //    let viewModel = HomeViewModel(apiService: <#T##APIServiceType#>)
 
+        let apiService = MockAPIService()
+        apiService.stub(for: SearchRepositoryRequest.self) { _ in
+            Result.Publisher(
+                SearchRepositoryResponse(
+                                    items: [
+                    .init(id: 0,
+                          name: "takeshi",
+                          description: "brabrabara",
+                          stargazersCount: 100,
+                          language: "Swift", url: "https://example.com",
+                          owner: Owner(id: 1,
+                                       avatarUrl: "https://example.com/photo.png"))
+                                    ]
+                                )
+            ).eraseToAnyPublisher()
+        }
+
+        let viewModel = HomeViewModel(apiService: apiService)
+        viewModel.apply(inputs: .onEnter(text: "sss"))
+        XCTAssertTrue(!viewModel.repositories.isEmpty)
     }
 
+    func testOnError() {
+        let apiService = MockAPIService()
+        apiService.stub(for: SearchRepositoryRequest.self) { _ in
+            Result.Publisher(
+                APIServiceError.responseError
+            ).eraseToAnyPublisher()
+        }
+        let viewModel = HomeViewModel(apiService: apiService)
+        viewModel.apply(inputs: .onEnter(text: "sss"))
+        XCTAssertTrue(viewModel.isShowError)
+    }
 
-
+    func testTappedErrorAlert() {
+        let apiService = MockAPIService()
+        apiService.stub(for: SearchRepositoryRequest.self) { _ in
+            Result.Publisher(
+                APIServiceError.responseError
+            ).eraseToAnyPublisher()
+        }
+        let viewModel = HomeViewModel(apiService: apiService)
+        viewModel.apply(inputs: .tappedErrorAlert)
+        XCTAssertTrue(!viewModel.isShowError)
+    }
 }
--- GitHubApiClientSample/GitHubApiClientSampleTests/MockAPIService.swift
+++ GitHubApiClientSample/GitHubApiClientSampleTests/MockAPIService.swift
@@ -10,3 +10,23 @@ import Foundation
 import Combine
 @testable import GitHubApiClientSample
 
+final class MockAPIService: APIServiceType {
+    var stubs: [Any] = []
+
+    typealias Request = APIRequestType
+
+    func stub<Request>(for type: Request.Type, response: @escaping ((Request) -> AnyPublisher<Request.Response, APIServiceError>)) where Request: APIRequestType {
+        stubs.append(response)
+    }
+
+    func response<Request>(from request: Request) -> AnyPublisher<Request.Response, APIServiceError> where Request: APIRequestType {
+
+        let response = stubs.compactMap { stub -> AnyPublisher<Request.Response, APIServiceError>? in
+            let stub = stub as? ((Request) -> AnyPublisher<Request.Response, APIServiceError>)
+            return stub?(request)
+        }.last
+
+        return response ?? Empty<Request.Response, APIServiceError>()
+            .eraseToAnyPublisher()
+    }
+}
