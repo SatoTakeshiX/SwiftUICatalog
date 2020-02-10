8ea8ed0fb070eb9f550e7184b39d3c1b849b5812
satoutakeshi <sato.takeshi1987@gmail.com>
2020-02-10 11:55:12 +0900

    add image sample

---
+++ BasicViews/BasicViews/Views/ImageSample.swift
@@ -0,0 +1,53 @@
+//
+//  ImageSample.swift
+//  BasicViews
+//
+//  Created by satoutakeshi on 2020/02/10.
+//  Copyright © 2020 satoutakeshi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ImageSample: View {
+    var body: some View {
+        ScrollView {
+        VStack(spacing: 16) {
+            Image("wing")
+                .frame(width: 80, height: 100)
+
+            Image(systemName: "moon")
+                .resizable()
+                .frame(width: 70, height: 100)
+
+            Image(systemName: "moon")
+             .resizable()
+            .aspectRatio(contentMode: .fill)
+             .frame(width: 70, height: 100)
+            Image(systemName: "moon")
+            .resizable()
+
+                .aspectRatio(contentMode: .fit)
+            .frame(width: 80, height: 100)
+
+            Image(systemName: "sunset.fill")
+            .resizable()
+            .renderingMode(.template)
+                .aspectRatio(contentMode: .fit)
+            .foregroundColor(Color.red)
+            .frame(width: 80, height: 100)
+
+            
+
+            }
+
+        }
+    .padding()
+        .frame(width: UIScreen.main.bounds.width)
+    }
+}
+
+struct ImageSample_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageSample()
+    }
+}
