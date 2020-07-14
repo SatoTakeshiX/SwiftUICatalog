ea16e889f84a9db9d04a7d39263146bf5802997a
satoutakeshi <sato.takeshi1987@gmail.com>
2020-07-14 23:41:45 +0900

    FilterPreviewContentView

--- CameraSample/CameraSample/Content/FilterBanner/FilterPreviewView.swift
+++ CameraSample/CameraSample/Content/FilterBanner/FilterPreviewView.swift
@@ -9,13 +9,28 @@
 import SwiftUI
 
 struct FilterPreviewContentView: View {
+    @Binding var isShowBanner: Bool
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            HStack {
+                Spacer()
+                Text("filter preview")
+                Spacer()
+            }
+            .frame(height: 120)
+                // おー、color. opacityで背景色だけ透明に
+                //https://stackoverflow.com/questions/58805852/how-can-i-make-a-background-in-swiftui-translucent
+            .background(Color.black.opacity(0.5))
+                //背景は透明でもなかのコンテンツはしっかり見えてほしい。
+            .foregroundColor(.yellow)
+                .offset(x: 0, y: isShowBanner ? 0 : 120)
+                .animation(.easeIn)
+        }
     }
 }
-
 struct FilterPreviewView_Previews: PreviewProvider {
     static var previews: some View {
-        FilterPreviewContentView()
+        FilterPreviewContentView(isShowBanner: .constant(true))
     }
 }
--- CameraSample/CameraSample/Content/FilterContentView.swift
+++ CameraSample/CameraSample/Content/FilterContentView.swift
@@ -10,56 +10,65 @@ import SwiftUI
 
 struct FilterContentView: View {
     @State private var isShowBanner = false
+    @State private var image: Image? = Image("snap")
     var body: some View {
         NavigationView {
             ZStack {
-                Rectangle()
+                // https://books.google.co.jp/books?id=S8DPDwAAQBAJ&pg=PT356&lpg=PT356&dq=swiftui+empty+view&source=bl&ots=_BhewlaJya&sig=ACfU3U1-vNwGbgAXhI_YHLc1ECDUhhNGxQ&hl=ja&sa=X&ved=2ahUKEwiqpLWT8MzqAhVCFogKHdkeBiUQ6AEwBnoECAoQAQ#v=onepage&q=swiftui%20empty%20view&f=false
+
+                // emptyviewはサイズがないview。
+                // rectangleと見比べた。rectangleのほうが子ビューを生成していたから、emptyを使っていこう
+
+
+                EmptyView()
+
                     //https://www.hackingwithswift.com/quick-start/swiftui/how-to-control-the-tappable-area-of-a-view-using-contentshape
-                    .contentShape(Rectangle())
-                    
-                   .foregroundColor(.clear)
-                   //.border(Color.red, width: 4)
-                    .onTapGesture {
-                        withAnimation {
-                            print("ddd")
-                            self.isShowBanner.toggle()
-                        }
-                }
-                .navigationBarTitle("Filter App")
-                .navigationBarItems(leading: EmptyView(), trailing: HStack {
-                    Button(action: {
-                        print("square.and.arrow.down")
-                    }, label: {
-                        Image(systemName: "square.and.arrow.down")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 30, height: 30)
-                    })
-                    Button(action: {
-                        print("photo")
-                    }, label: {
-                        Image(systemName: "photo")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 30, height: 30, alignment: .bottom)
-                    })
-                })
 
-                if isShowBanner {
-                    VStack {
+                    .navigationBarTitle("Filter App")
+                    .navigationBarItems(leading: EmptyView(), trailing: HStack {
+                        Button(action: {
+                            print("square.and.arrow.down")
+                        }, label: {
+                            Image(systemName: "square.and.arrow.down")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 30, height: 30)
+                        })
+                        Button(action: {
+                            print("photo")
+                        }, label: {
+                            Image(systemName: "photo")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 30, height: 30, alignment: .bottom)
+                        })
+                    })
+
+                if image != nil {
+                    HStack {
                         Spacer()
-                        HStack {
-                            Spacer()
-                            Text("filter preview")
-                            Spacer()
-                        }
-                        .frame(height: 130)
-                        .background(Color.red)
-                        .foregroundColor(.yellow)
+                        image?
+                            .resizable()
+                            // fillだとタップイベントがきかない？
+                            .aspectRatio(contentMode: .fit)
+                        Spacer()
 
-                        .transition(.move(edge: .bottom))
+                    }
+                    .border(Color.green, width: 4)
+                        .contentShape(Rectangle())
+                        .onTapGesture {
+                            withAnimation {
+                                print("ddd")
+                                self.isShowBanner.toggle()
+                            }
                     }
                 }
+
+              //  if isShowBanner {
+                FilterPreviewContentView(isShowBanner: $isShowBanner)
+               // }
+
+                // transitionでアニメーションするよりは.offsetで移動させたほうがきれいなアニメーションになるな
             }
         }
     }
