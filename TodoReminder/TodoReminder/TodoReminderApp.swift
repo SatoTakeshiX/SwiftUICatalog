d7660af081d8ece0bca441dd1c7aa3a3d31570be
satoutakeshi <sato.takeshi1987@gmail.com>
2020-08-11 09:45:33 +0900

    add todo project

---
+++ TodoReminder/TodoReminder/TodoReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoReminderApp.swift
+//  TodoReminder
+//
+//  Created by satoutakeshi on 2020/08/11.
+//
+
+import SwiftUI
+
+@main
+struct TodoReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
