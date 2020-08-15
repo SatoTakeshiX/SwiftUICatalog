a875f3853dee5b4f2698b280400ac9d598de15da
satoutakeshi <sato.takeshi1987@gmail.com>
2020-08-16 01:06:09 +0900

    add widget views

---
+++ TodoReminder/TodoWidget/DateListView.swift
@@ -0,0 +1,20 @@
+//
+//  DateListView.swift
+//  TodoReminder
+//
+//  Created by satoutakeshi on 2020/08/16.
+//
+
+import SwiftUI
+
+struct DateListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DateListView_Previews: PreviewProvider {
+    static var previews: some View {
+        DateListView()
+    }
+}
---
+++ TodoReminder/TodoWidget/TodoSingleView.swift
@@ -0,0 +1,20 @@
+//
+//  TodoSingleView.swift
+//  TodoReminder
+//
+//  Created by satoutakeshi on 2020/08/16.
+//
+
+import SwiftUI
+
+struct TodoSingleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TodoSingleView_Previews: PreviewProvider {
+    static var previews: some View {
+        TodoSingleView()
+    }
+}
