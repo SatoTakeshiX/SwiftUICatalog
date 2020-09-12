3dc7c98a8c43a89c40b81339ebf9d944c1605071
t-sato <sato.takeshi1987@gmail.com>
2020-09-12 16:33:10 +0900

    remove comments

--- BasicViews/BasicViews/Views/iOS14/ProgressViewSample.swift
+++ BasicViews/BasicViews/Views/iOS14/ProgressViewSample.swift
@@ -8,37 +8,6 @@
 
 import SwiftUI
 
-// 文字を乗せれるのと、timerで進行できるのの２つを紹介しよう
-// https://www.hackingwithswift.com/quick-start/swiftui/how-to-show-progress-on-a-task-using-progressview
-
-
-/**
- /// Creates a progress view for showing determinate progress that generates
- /// its label from a localized string.
- ///
- /// If the value is non-`nil`, but outside the range of `0.0` through
- /// `total`, the progress view pins the value to those limits, rounding to
- /// the nearest possible bound. A value of `nil` represents indeterminate
- /// progress, in which case the progress view ignores `total`.
- ///
- /// This initializer creates a ``Text`` view on your behalf, and treats the
- /// localized key similar to ``Text/init(_:tableName:bundle:comment:)``. See
- /// ``Text`` for more information about localizing strings. To initialize a
- ///  determinate progress view with a string variable, use
- ///  the corresponding initializer that takes a `StringProtocol` instance.
- ///
- /// - Parameters:
- ///     - titleKey: The key for the progress view's localized title that
- ///       describes the task in progress.
- ///     - value: The completed amount of the task to this point, in a range
- ///       of `0.0` to `total`, or `nil` if the progress is
- ///       indeterminate.
- ///     - total: The full amount representing the complete scope of the
- ///       task, meaning the task is complete if `value` equals `total`. The
- ///       default value is `1.0`.
- public init<V>(_ titleKey: LocalizedStringKey, value: V?, total: V = 1.0) where Label == Text, CurrentValueLabel == EmptyView, V : BinaryFloatingPoint
-
- */
 @available(iOS 14.0, *)
 struct ProgressViewSample: View {
     var body: some View {
