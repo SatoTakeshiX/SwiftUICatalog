f34bcba6674e1e34d5b3e07c4a6ce788ec61963c
t-sato <sato.takeshi1987@gmail.com>
2020-09-14 01:02:37 +0900

    app scene

--- AppSceneSample/AppSceneSample/AppSceneSampleApp.swift
+++ AppSceneSample/AppSceneSample/AppSceneSampleApp.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 @main
 struct MyApp: App {
+    @Environment(\.scenePhase) private var scenePhase
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(colums: adaptiveColums)
+                .onChange(of: scenePhase) { newScenePhase in
+                    switch newScenePhase {
+                    case .background:
+                        print("do something when background")
+                    case .inactive:
+                        print("do something when inactive")
+                    case .active:
+                        print("do something when active")
+                    @unknown default:
+                        fatalError()
+                    }
+                }
         }
     }
 }
--- AppSceneSample/AppSceneSample/ContentView.swift
+++ AppSceneSample/AppSceneSample/ContentView.swift
@@ -6,16 +6,27 @@
 //
 
 import SwiftUI
+let adaptiveColums = [
+    GridItem(.adaptive(minimum: 100), spacing: 0)
+]
 
 struct ContentView: View {
+    let colums: [GridItem]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            LazyVGrid(columns: colums, spacing: 0) {
+                ForEach(0 ..< 100) { _ in
+                    Image("seal_\(Int.random(in: 1 ... 3))")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(colums: adaptiveColums)
     }
 }
