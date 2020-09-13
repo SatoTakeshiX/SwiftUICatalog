6bf15cd70ef7ba8943d106fb40c9a57eced20212
t-sato <sato.takeshi1987@gmail.com>
2020-09-13 22:52:28 +0900

    add scene

---
+++ AppSceneSample/AppSceneSample/AppSceneSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppSceneSampleApp.swift
+//  AppSceneSample
+//
+//  Created by t-sato on 2020/09/12.
+//
+
+import SwiftUI
+
+@main
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppSceneSample/AppSceneSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppSceneSample
+//
+//  Created by t-sato on 2020/09/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
