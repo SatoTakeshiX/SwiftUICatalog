fa5407d175562b8ff57e6308717a02fa199b9af8
satoutakeshi <sato.takeshi1987@gmail.com>
2020-10-03 21:46:52 +0900

    open detail screen

--- TodoReminder/TodoWidget/TodoWidget.swift
+++ TodoReminder/TodoWidget/TodoWidget.swift
@@ -11,19 +11,19 @@ import SwiftUI
 struct Provider: TimelineProvider {
 
     func placeholder(in context: Context) -> RecentTodoEntry {
-        RecentTodoEntry(date: Date(), title: "Widget開発", priority: 2)
+        RecentTodoEntry(date: Date(), title: "Widget開発", priority: 2, id: UUID())
     }
 
     func getSnapshot(in context: Context, completion: @escaping (RecentTodoEntry) -> ()) {
         if context.isPreview {
-            let entry = RecentTodoEntry(date: Date(), title: "Widget開発", priority: 2)
+            let entry = RecentTodoEntry(date: Date(), title: "Widget開発", priority: 2, id: UUID())
             completion(entry)
         } else {
             do {
                 let store = TodoListStore()
                 let todoLists = try store.fetchTodayItems()
                 let entries = todoLists.map { (todoList) -> RecentTodoEntry in
-                    RecentTodoEntry(todo: todoList)
+                    RecentTodoEntry(todoItem: todoList)
                 }
                 guard let first = entries.first else {
                     return
@@ -40,7 +40,7 @@ struct Provider: TimelineProvider {
             let store = TodoListStore()
             let todoLists = try store.fetchTodayItems()
             let entries = todoLists.map { (todoList) -> RecentTodoEntry in
-                RecentTodoEntry(todo: todoList)
+                RecentTodoEntry(todoItem: todoList)
             }
             let timeline = Timeline(entries: entries, policy: .atEnd)
             completion(timeline)
@@ -58,17 +58,20 @@ struct RecentTodoEntry: TimelineEntry {
     let date: Date
     let title: String
     let priority: Int
+    let id: UUID
 
-    init(date: Date, title: String, priority: Int) {
+    init(date: Date, title: String, priority: Int, id: UUID) {
         self.date = date
         self.title = title
         self.priority = priority
+        self.id = id
     }
 
-    init(todo: TodoListItem) {
-        self.date = todo.startDate
-        self.title = todo.title
-        self.priority = todo.priority
+    init(todoItem: TodoListItem) {
+        self.date = todoItem.startDate
+        self.title = todoItem.title
+        self.priority = todoItem.priority
+        self.id = todoItem.id
     }
 }
 
@@ -80,7 +83,6 @@ struct TodoWidgetEntryView : View {
         VStack {
             HStack {
                 Rectangle()
-                    .frame(width: .infinity, height: .infinity)
                     .foregroundColor(makePriorityColor(priority: entry.priority))
                     .clipShape(ContainerRelativeShape())
                     .overlay (
@@ -99,6 +101,7 @@ struct TodoWidgetEntryView : View {
             }
         }
         .padding(8)
+        .widgetURL(makeURLScheme(id: entry.id))
     }
 
     private func makePriorityColor(priority: Int) -> Color {
@@ -113,6 +116,14 @@ struct TodoWidgetEntryView : View {
                 return .black
         }
     }
+
+    func makeURLScheme(id: UUID) -> URL {
+        let url = URL(string: "todolist://detail")!
+        var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true)
+        urlComponents?.queryItems = [URLQueryItem(name: "id", value: id.uuidString)]
+        print("urlcom \(urlComponents)")
+        return urlComponents!.url!
+    }
 }
 
 @main
@@ -124,6 +135,7 @@ struct TodoWidget: Widget {
         // 編集がないWidget
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
             TodoWidgetEntryView(entry: entry)
+
         }
         // Widget gallary のタイトル
         .configurationDisplayName("Todo Reminder")
@@ -135,7 +147,7 @@ struct TodoWidget: Widget {
 
 struct TodoWidget_Previews: PreviewProvider {
     static var previews: some View {
-        TodoWidgetEntryView(entry: RecentTodoEntry(date: Date(), title: "Widget開発", priority: 2))
+        TodoWidgetEntryView(entry: RecentTodoEntry(date: Date(), title: "Widget開発", priority: 2, id: UUID()))
             // previewContextでwidgetの大きさをプレビューできる
             .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
