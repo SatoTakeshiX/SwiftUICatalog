30730745dc2e1c01f36861a4c6c51a870e9addf7
satoutakeshi <sato.takeshi1987@gmail.com>
2020-10-16 09:13:45 +0900

    remove comment

--- DataFlowSample.playground/Pages/PropertySample.xcplaygroundpage/Contents.swift
+++ DataFlowSample.playground/Pages/PropertySample.xcplaygroundpage/Contents.swift
@@ -24,11 +24,3 @@ struct ChildView: View {
 PlaygroundPage.current.setLiveView(ParentView()
                                     .frame(width: 600, height: 400).padding())
 
-
-/*
- SwiftUIのデータを扱う上での３つの質問
- ・jobを行うために必要なデータはなにか？（値型か参照型か）データは種類。タイトル、進捗状況など
- ・ビューはデータをどのように処理するか。参照だけか、更新するか
- ・データはどこから来るか。親ビューからか。自分で作るのか
- */
-
