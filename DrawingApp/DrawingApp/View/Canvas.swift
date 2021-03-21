f1886ea95bcd1e1a21005f0c8c70bc1c96ee1dac
satoutakeshi <sato.takeshi1987@gmail.com>
2021-03-21 23:31:08 +0900

    filterDistanceを追加

--- DrawingApp/DrawingApp/View/Canvas.swift
+++ DrawingApp/DrawingApp/View/Canvas.swift
@@ -40,8 +40,16 @@ struct Canvas: View {
             .gesture(
                 DragGesture(minimumDistance: 0)
                     .onChanged({ (value) in
-                        tmpDrawPoints.points.append(value.location)
                         tmpDrawPoints.color = selectedColor.color
+                        guard !tmpDrawPoints.points.isEmpty else {
+                            tmpDrawPoints.points.append(value.location)
+                            return
+                        }
+
+                        if let lastPoint = tmpDrawPoints.points.last,
+                           filterDistance(startPoint: lastPoint, endPoint: value.location) {
+                            tmpDrawPoints.points.append(value.location)
+                        }
                     })
                     .onEnded({ (value) in
                         endedDrawPoints.append(tmpDrawPoints)
@@ -49,5 +57,15 @@ struct Canvas: View {
                     })
             )
         }
+    }
+
+    /// 座標の距離が近いかどうかを判定する。複数本の指をタップした場合もDragGestureはonChangedを呼ぶが、連続した線ではないのでフィルターをかける。
+    /// - Parameters:
+    ///   - startPoint: 開始座標
+    ///   - endPoint: 終わりの座標
+    /// - Returns: 距離が130以下ならtrue, それ以外ならfalse
+    private func filterDistance(startPoint: CGPoint, endPoint: CGPoint) -> Bool {
+        let distance = sqrt(pow(Double(startPoint.x) - Double(endPoint.x), 2) + pow(Double(startPoint.y) - Double(endPoint.y), 2))
+        return distance <= 130
     }
 }
